6c6b4abfa2c64213809f448d680a5855f0003162
Zoe J <hira4fun@gmail.com>
2023-02-21 16:43:04 -0600

    Basic Scene and Movement

---
+++ Assets/Scripts/charMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+        rb.velocity = movement * moveSpeed;
+    }
+}
