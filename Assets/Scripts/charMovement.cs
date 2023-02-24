0a3ed3201321fd302184a608b2b11312c90bfd50
Zoe J <hira4fun@gmail.com>
2023-02-24 03:11:26 -0600

    added dialogue boxes

--- Assets/Scripts/charMovement.cs
+++ Assets/Scripts/charMovement.cs
@@ -7,20 +7,26 @@ public class charMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Animator animator;
     private Rigidbody2D rb;
+    public static bool pause = false;
 
-    private void Start()
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+         pause = false; 
+         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(horizontalInput, verticalInput);
-        rb.velocity = movement * moveSpeed;
-        animator.SetFloat("Speed", rb.velocity.magnitude);
+        if (!pause)
 
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2(horizontalInput, verticalInput);
+            rb.velocity = movement * moveSpeed;
+            animator.SetFloat("Speed", rb.velocity.magnitude);
+        }
 
     }
 }
