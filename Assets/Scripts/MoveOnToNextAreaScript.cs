3cdb531d71bda85ad23ae7d2b6168fc75d3298ae
Zoe J <hira4fun@gmail.com>
2023-03-10 07:05:06 -0600

    audio

--- Assets/Scripts/MoveOnToNextAreaScript.cs
+++ Assets/Scripts/MoveOnToNextAreaScript.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class MoveOnToNextAreaScript : Trigger
 {
     GameObject instance;
-    bool isTrigger = false;
     public BoxCollider2D box;
     public GameObject boxSprite;
 
     public override void Action()
     {
-        isTrigger = true;
         box.isTrigger = true;
         boxSprite.SetActive(true);
 
--- Assets/Scripts/closeDoor.cs
+++ Assets/Scripts/closeDoor.cs
@@ -6,13 +6,11 @@ public class closeDoor : Trigger
 {
 
     GameObject instance;
-    bool isTrigger = true;
     public BoxCollider2D box;
     public GameObject boxSprite;
 
     public override void Action()
     {
-        isTrigger = false;
         box.isTrigger = false;
         boxSprite.SetActive(false);
 
