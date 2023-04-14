3e177e12556abb2000d669f8f7067e1844752d5e
Collin <collinsomerville1@gmail.com>
2023-04-14 07:36:00 -0500

    Added slime enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public SlimeDetection slimeDetection;
+
+    public float moveSpeed = 1000f;
+
+    Rigidbody2D rb;
+
+    void FixedUpdate(){
+        if(slimeDetection.detectedObjs.Count > 0){
+            // Calculate direction to target object
+            Vector2 direction = (slimeDetection.detectedObjs[0].transform.position - transform.position).normalized;
+
+            // Move towards detected object
+            rb.AddForce(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+
     public float Health {
         set {
             health = value;
@@ -20,7 +37,7 @@ public class Enemy : MonoBehaviour
     public float health = 1;
 
     private void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void Defeated() {
--- Assets/Scripts/Player Scripts/WhipAttack.cs
+++ Assets/Scripts/Player Scripts/WhipAttack.cs
@@ -36,5 +36,6 @@ public class WhipAttack : MonoBehaviour
                 enemy.Health -= damage;
             }
         }
+
     }
 }
